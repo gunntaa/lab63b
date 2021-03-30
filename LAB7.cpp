d7e8dd7a4549d436ef3c36831115b77d4a57ff60
gunntaa <80879351+gunntaa@users.noreply.github.com>
2021-03-30 21:08:38 +0700

    Update LAB7.cpp

--- LAB7.cpp
+++ LAB7.cpp
@@ -13,10 +13,8 @@ void loop()
 {
 Serial.println("========== ON ===========");
 digitalWrite(0, HIGH);
-delay(1000*1000)   // ทำงาน 1 นาที     
+delay(60*1000)   // ทำงาน 1 นาที     
 Serial.println("========== OFF ===========");
 digitalWrite(0, LOW);
 delay(7200*1000) // หยุดทำงาน 120 นาที
 }
-
-
